75e5475cd5de6cda55362460776fb1c4e70ef4cf
MikaelPL1 <mikaelpl151@gmail.com>
2025-09-23 11:00:58 +0300

    users

--- part4/blogisovellus/app.js
+++ part4/blogisovellus/app.js
@@ -2,6 +2,8 @@ const express = require('express')
 const mongoose = require('mongoose')
 const config = require('./utils/config')
 const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
+
 
 const app = express()
 
@@ -16,5 +18,7 @@ mongoose.connect(config.MONGODB_URI)
 app.use(express.json())
 
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
+
 
 module.exports = app
--- part4/blogisovellus/controllers/blogs.js
+++ part4/blogisovellus/controllers/blogs.js
@@ -1,5 +1,7 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
+
 
 // GET all blogs
 blogsRouter.get('/', (request, response) => {
@@ -9,12 +11,28 @@ blogsRouter.get('/', (request, response) => {
 })
 
 // POST new blog
-blogsRouter.post('/', (request, response) => {
-  const blog = new Blog(request.body)
+blogsRouter.post('/', async (request, response) => {
+  const body = request.body
+  const user = await User.findById(body.userId)
+  if (!user) {
+    return response.status(400).json({ error: 'userId missing or not valid' })
+  }
 
-  blog.save().then((result) => {
-    response.status(201).json(result)
+  const blog = new Blog({
+    title: body.title,
+    author: {
+      name: user.name,
+      id: user._id
+    },
+    url: body.url,
+    likes: body.likes || 0
   })
+
+  const savedBlog = await blog.save()
+  user.blogs = user.blogs.concat(savedBlog._id)
+  await user.save()
+
+  response.status(201).json(savedBlog)
 })
 
 blogsRouter.delete('/:id', async (request, response) => {
